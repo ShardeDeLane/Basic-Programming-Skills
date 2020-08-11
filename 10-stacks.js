e22c68a27f23261c794c2439b25b25246aba594e
Sharde DeLane <rhcprocker@yahoo.com>
2020-08-10 22:48:07 -0500

    Update 10-stacks.js

--- 10-stacks.js
+++ 10-stacks.js
@@ -1,1 +1,34 @@
 
+// code out stack
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;
+	}
+	// remove
+	pop() {
+		
+	}
+	push(data) {
+		const node = new Node(data);
+		if(!this.top) {
+			this.top = node;
+			this.bottom = node;
+			this.length++;
+		} else {
+			let temp = this.top;
+			this.top = node;
+			this.top.next = temp;
+			this.length++;
+		}
+	}
+	peek() {}
+}
